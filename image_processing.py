dcef6041a428838c0f08aae4a550d429cee60208
Laroussestela <62725709+Laroussestela@users.noreply.github.com>
2025-04-25 11:09:45 +0200

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,9 @@
 import nibabel as nib
 import numpy as np
 import os
+import shutil
+import random
+from pathlib import Path
 import cv2
 from tqdm import tqdm
 
@@ -106,4 +109,39 @@ def create_txt_yolo():
                         f.write(f"{clase} {x_c:.6f} {y_c:.6f} {w:.6f} {h:.6f}\n")
             else:
                 open(path_txt, "w").close()
+
+def create_path():
+    src_images = Path("imagenes")
+    src_labels = Path("labels/yolov11")
+
+    base_dir = Path("data_yolo")
+    image_train_dir = base_dir / "images" / "train"
+    image_val_dir   = base_dir / "images" / "val"
+    label_train_dir = base_dir / "labels" / "train"
+    label_val_dir   = base_dir / "labels" / "val"
+
+    for d in [image_train_dir, image_val_dir, label_train_dir, label_val_dir]:
+        d.mkdir(parents=True, exist_ok=True)
+
+    archivos = [f.stem for f in src_images.glob("*.jpg")]
+    random.shuffle(archivos)
+
+    split_idx = int(0.8 * len(archivos))
+    train_archivos = archivos[:split_idx]
+    val_archivos = archivos[split_idx:]
+
+    def mover_archivos(archivos, image_dst, label_dst):
+        for name in archivos:
+            img_path = src_images / f"{name}.jpg"
+            label_path = src_labels / f"{name}.txt"
+
+            if img_path.exists():
+                shutil.copy(img_path, image_dst / f"{name}.jpg")
+            if label_path.exists():
+                shutil.copy(label_path, label_dst / f"{name}.txt")
+            else:
+                open(label_dst / f"{name}.txt", 'w').close()
+
+    mover_archivos(train_archivos, image_train_dir, label_train_dir)
+    mover_archivos(val_archivos, image_val_dir, label_val_dir)
 
