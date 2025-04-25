cf4a7080ad94bdcb9d8a25ef1353936188fafd0d
Laroussestela <62725709+Laroussestela@users.noreply.github.com>
2025-04-25 11:13:15 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
-data_yaml = 'data.yaml'
+def train_model():
+    data_yaml = 'data.yaml'
+    model = YOLO('yolo11n.pt')
 
-model = YOLO('yolo11n.pt')
-
-results = model.train(
-    data=data_yaml,
-    epochs=100,
-    patience=7,
-    project='runs_yolov11',
-    name='tumor_detector'
-)
+    results = model.train(
+        data=data_yaml,
+        epochs=100,
+        patience=7,
+        project='runs_yolov11',
+        name='tumor_detector'
+    )
